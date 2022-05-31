6a6f2a1c560f1bc94d03dc89633a042dae9871ee
matka00 <anita.skumat@gmail.com>
2022-05-31 12:34:45 +0200

    automatic saving added

---
+++ frontend/save-docs.js
@@ -0,0 +1,23 @@
+setInterval(save, 3000);
+
+async function save(){
+    const textarea = document.querySelector('#doc-text');
+    const docText = textarea.value;
+
+    const saveText = document.querySelector('#save-in-progress-text');
+    saveText.classList.remove('hidden');
+
+/*     fetch('http://127.0.0.1:9000/', {
+        method: 'POST',
+        body: docText
+    }).then(function (){
+        saveText.classList.add('hidden');
+    }); */
+
+    await fetch('http://127.0.0.1:9000/', {
+        method: 'POST',
+        body: docText
+    });
+    saveText.classList.add('hidden');
+
+};
